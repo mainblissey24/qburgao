afc23c0241e631f73ee9709d37c3262be0ba1d60
mainblissey124 <mimimkyugx100@gmail.com>
2023-09-27 16:54:52 -0300

    alt

--- frontend/src/pages/Produto_Pedido/index.js
+++ frontend/src/pages/Produto_Pedido/index.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { Form, Container } from "./style";
 import api from "../../services/api";
 import Logo from "../../assets/senac.png";
@@ -13,65 +13,97 @@ const Produto_Pedido = () => {
 
     const [produtos, setTableProdutos] = useState([]);
     const [pedidos, setTablePedidos] = useState([]);
+
     const [error, setError] = useState("");
 
     const navigate = useNavigate();
+    const { id } = useParams();
 
     useEffect(() => {
-        async function getData(){
+        async function getSelect(){
+            try{
+                 //Carrega combobox produtos
+            let response = await api.get('/produtos');
+            setTableProdutos(response.data);
+
+            //Carrega combobox pedidos
+            response = await api.get('/pedidos');
+            setTablePedidos(response.data);
+            }catch(err){
+                setError("Houve um erro ao encontar pedido e produto")
+            }
+        }
+        getSelect();
+        if (!id) return;
+
+        async function getData() {
+            try {
+                const { data } = await api.get(`/produtos_pedidos/${id}`);
+                setIdProduto(data.produtos_idprodutos);
+                setIdPedido(data.pedidos_idpedidos);
+                setObservacao(data.observacao);
+            } catch (err) {
+                setError("Houve um problema ao carregar os dados do produto: " + err);
+            }
             //Carrega combobox produtos
             let response = await api.get('/produtos');
             setTableProdutos(response.data);
-                        //Carrega combobox pedidos
-                        response = await api.get('/pedidos');
-                        setTablePedidos(response.data);
-                        console.log(pedidos);
+
+            //Carrega combobox pedidos
+            response = await api.get('/pedidos');
+            setTablePedidos(response.data);
         }
         getData();
     }, []);
 
     const handleProduto = async e => {
         e.preventDefault();
-        if (!produtos_idprodutos || !pedidos_idpedidos || !observacao){
+        if (!produtos_idprodutos || !pedidos_idpedidos || !observacao) {
             setError("Preencha todos os dados para cadastrar");
         } else {
             try {
-                await api.post("/produtos_pedidos", {produtos_idprodutos, pedidos_idpedidos, observacao});
+                await api.post("/produtos_pedidos", { produtos_idprodutos, pedidos_idpedidos, observacao });
                 navigate("/produtos_pedidos");
-            } catch (err){
+            } catch (err) {
                 console.log(err);
                 setError("Ocorreu um erro ao cadastrar produtos_pedidos.")
             }
         }
     }
 
-return (
-    <div>
-        <Navbar />
-        <Container>
-            <Form onSubmit={handleProduto}>
-                {error && <p>{error}</p>}
-                <img src={Logo} alt="logo_senac"/>
-                <input
-                    type="number"
-                    placeholder="ID Produto"
-                    onChange={e => setIdProduto(e.target.value)}
-                />  
-                <input
-                    type="number"
-                    placeholder="ID Pedido"
-                    onChange={e => setIdPedido(e.target.value)}
-                />   
-                <input 
-                    type="text"
-                    placeholder="Observação"
-                    onChange={e => setObservacao(e.target.value)}
-                />
-                <button type="submit">Cadastro de Produtos_Pedidos</button>
-            </Form>
-        </Container>
-    </div>    
-)
+    return (
+        <div>
+            <Navbar />
+            <Container>
+                <Form onSubmit={handleProduto}>
+                    {error && <p>{error}</p>}
+                    <img src={Logo} alt="logo_senac" />
+                    <select onChange={e => setIdProduto(e.target.value)} value={produtos_idprodutos}>
+                        <option value="">Selecione um Produto</option>
+                        {produtos.map(produto =>(
+                            <option key={produtos.idprodutoss} value={produtos.idprodutoss}>
+                                {produto.idprodutos}
+                            </option>
+                        ))}
+                    </select>
+                    <select onChange={e => setIdPedido(e.target.value)} value={pedidos_idpedidos}>
+                        <option value="">Selecione um Pedido</option>
+                        {pedidos.map(pedido =>(
+                            <option key={pedido.idpedidos} value={pedido.idpedidos}>
+                                {pedido.idpedidos}
+                            </option>
+                        ))}
+                    </select>
+                    <input
+                        type="text"
+                        placeholder="Observação"
+                        onChange={e => setObservacao(e.target.value)}
+                    />
+                    <button type="submit">Cadastro de Produtos_Pedidos</button>
+                </Form>
+            </Container>
+        </div>
+    )
 }
 
 export default Produto_Pedido;
